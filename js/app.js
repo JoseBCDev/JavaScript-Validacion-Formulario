e852491eb38c7c119dc6208d6879a4a75a18a7d9
xdgonex <bendezucortezjoseluis@gmail.com>
2022-09-05 19:19:17 -0500

    Validacion de Campos

--- js/app.js
+++ js/app.js
@@ -2,11 +2,21 @@
 
 btnEnviar = document.querySelector('#enviar');
 
+email = document.querySelector('#email');
+asunto = document.querySelector('#asunto');
+mensaje = document.querySelector('#mensaje');
+
 eventListeners(); //FUNCION SE INICIA, CUANDO SE CARGA LA PAGINA
 
 //FUNCION CREADA PARA EL ARRANQUE DE LA PAGINA
 function eventListeners(){
-    document.addEventListener('DOMContentLoaded',IniciarApp)
+    document.addEventListener('DOMContentLoaded',IniciarApp);
+
+    email.addEventListener('blur',validarFormulario);
+
+    asunto.addEventListener('blur',validarFormulario);
+
+    mensaje.addEventListener('blur',validarFormulario);
 }
 //FUNCIONES
 
@@ -14,6 +24,15 @@ function IniciarApp()
 {
     btnEnviar.disabled = true;
     btnEnviar.classList.add('cursor-not-allowed','opacity-50');
-    console.log(btnEnviar);
+}
+
+function validarFormulario(e)
+{
+    if(e.target.value.length > 0){
+         console.log('no hay algo');
+    }else{
+        e.target.classList.add('border','border-red-500');
+    }
+ 
 }
 
