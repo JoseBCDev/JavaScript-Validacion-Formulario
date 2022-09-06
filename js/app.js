631b01027d6a47e20b48d04329d19a1b00339120
xdgonex <bendezucortezjoseluis@gmail.com>
2022-09-06 00:24:08 -0500

    Agregando Efecto Mensaje de Error

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
 //VARIABLES
 
-btnEnviar = document.querySelector('#enviar');
-
-email = document.querySelector('#email');
-asunto = document.querySelector('#asunto');
-mensaje = document.querySelector('#mensaje');
+const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
 
 eventListeners(); //FUNCION SE INICIA, CUANDO SE CARGA LA PAGINA
 
@@ -29,10 +29,27 @@ function IniciarApp()
 function validarFormulario(e)
 {
     if(e.target.value.length > 0){
-         console.log('no hay algo');
+         console.log('si hay algo');
     }else{
         e.target.classList.add('border','border-red-500');
+        mostrarError();
     }
- 
 }
 
+function mostrarError(){
+    //CREAMOS UN ELEMENTO P Y LE DAMOS TEXTO Y DISEÑO
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = 'Todos los campos son obligatorios';
+    mensajeError.classList.add('border','border-red-500','background-red-100','text-red-500','p-3','mt-5','text-center','error');
+
+    
+    //CALCULAMOS CUANTAS CLASES TIENE EL DOCUMENTO '.ERROR'
+    const errores = document.querySelectorAll('.error');
+    // SI TIENE MÁS DE 0 SIGNIFICA Q DIO VARIAS VECES, SINO SE AGREGA POR PRIMERA VEZ EL MENSAJE
+    if(errores.length === 0)
+    {
+        formulario.appendChild(mensajeError);
+    }else{
+
+    }
+}
